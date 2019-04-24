3a6ee4d23c0d1b5bf79ac2208b57bf1ac0ffcbb6
Michael Harvey <mikeytown19@gmail.com>
2019-04-24 07:03:51 -0600

    almost finished the accordion, need to make the toggles only work on the single item and not all of them

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -23,35 +23,50 @@ const Container = styled.div`
     background-color: ${Colors.primary};
     cursor: pointer;
     padding: 15px;
-  }
+   }
 
-  p {
-    padding: 15px;
+
+   p {
     display: none;
-  }
+    padding: 15px;
+   }
+
+   &.active {
+    p {
+      display: flex;
+    }
+   }
 
 
 `
 
+
 function Accordion() {
   const [opened, toggleOpened] = useState(0);
 
-  useEffect(() => {
+ const handleClick = (e) => {
+  if(opened);
+    e.currentTarget.classList.toggle('active')
+ }
 
-  })
+  useEffect((props) => {
+  }, [opened]);
 
     return (
       FaqData.map((item, index) => {
-        console.log(opened)
+
         return (
-          <Container key={index} onClick={()=> toggleOpened(opened + 1)}>
+          <Container key={index} onClick={(e)=> {
+            opened ? toggleOpened(opened - 1) : toggleOpened(opened + 1);
+            handleClick(e);
+          }}>
             <div>
             <h5>{item.title}</h5>
             {!opened ?
             <svg xmlns="http://www.w3.org/2000/svg" fill="white" width="24" height="24" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg> :
              <svg xmlns="http://www.w3.org/2000/svg" fill="white" width="24" height="24" viewBox="0 0 24 24"><path d="M0 10h24v4h-24z"/></svg> }
             </div>
-            <p>{item.answer}</p>
+            <p opened={opened}>{item.answer}</p>
           </Container>
         )
       })
