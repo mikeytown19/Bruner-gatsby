f5338f4f3e37cf15c772b7992df8ca32c12aa515
Michael Harvey <mikeytown19@gmail.com>
2019-04-25 12:50:04 -0600

    added in the start background

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ import Colors from '../styles/colors'
 import Wave from '../components/wave'
 import Accordions from '../components/accordion'
 import Footer from '../components/footer'
+import Bp from '../styles/breakpoints'
 
 
 const FlexWrap = styled.div`
@@ -175,28 +176,194 @@ const Card = ({children}) =>  (
 )
 
 
+const SkyContainer = styled.div`
+  * {
+    box-sizing: border-box;
+  }
+
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  position: relative;
+  height: 500px;
+
+  ${ContainerWrapper} {
+    width: 100%;
+    position: absolute;
+    padding-top: 0;
+    color: white;
+    text-align: center;
+
+    ${Bp.medium} {
+      padding-top: 75px;
+    }
+  }
+
+
+  .sky-container {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(125deg, #00103a 0%, #3e5f77 100%);
+
+}
+
+.border {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  box-shadow: 0 0 3px 2px #d6d6d6;
+  z-index: 100;
+}
+
+.star {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  height: 2px;
+  background: linear-gradient(-45deg, #5f91ff, rgba(0, 0, 255, 0));
+  filter: drop-shadow(0 0 6px #699bff);
+  animation: tail 3000ms ease-in-out infinite, shooting 3000ms ease-in-out infinite;
+}
+
+.star::before, .star::after {
+  position: absolute;
+  content: '';
+  top: calc(50% - 1px);
+  right: 0;
+  height: 2px;
+  background: linear-gradient(-45deg, rgba(0, 0, 255, 0), #5f91ff, rgba(0, 0, 255, 0));
+  border-radius: 100%;
+  transform: translateX(50%) rotateZ(45deg);
+  animation: shining 3000ms ease-in-out infinite;
+}
+
+.star::after {
+  transform: translateX(50%) rotateZ(-45deg);
+}
+
+.star:nth-child(1) {
+  top: calc(50% - 100px);
+  left: calc(25% - 250px);
+  animation-delay: 650ms;
+}
+.star:nth-child(1)::before, .star:nth-child(1)::after {
+  animation-delay: 650ms;
+}
+
+.star:nth-child(2) {
+  top: calc(50% + 50px);
+  left: calc(50% - 800px);
+  animation-delay: 150ms;
+}
+.star:nth-child(2)::before, .star:nth-child(2)::after {
+  animation-delay: 150ms;
+}
+
+.star:nth-child(3) {
+  top: calc(50%  + 20px);
+  left: calc(50% + 200px);
+  animation-delay: 1600ms;
+}
+.star:nth-child(3)::before, .star:nth-child(3)::after {
+  animation-delay: 1600ms;
+}
+
+.star:nth-child(4) {
+  top: calc(65% - 50px);
+  left: calc(50% - 250px);
+  animation-delay: 4700ms;
+}
+.star:nth-child(4)::before, .star:nth-child(4)::after {
+  animation-delay: 4700ms;
+}
+
+.star:nth-child(5) {
+  top: calc(50% - -190px);
+  left: calc(50% + 400px);
+  animation-delay: 2100ms;
+}
+.star:nth-child(5)::before, .star:nth-child(5)::after {
+  animation-delay: 2100ms;
+}
+
+
+
+
+/* Animations */
+@keyframes tail {
+  0% {
+    width: 0;
+  }
+
+  30% {
+    width: 100px;
+  }
+
+  100% {
+    width: 0;
+  }
+}
+
+@keyframes shining {
+  0% {
+    width: 0;
+  }
+  50% {
+    width: 30px;
+  }
+  100% {
+    width: 0;
+  }
+}
+
+@keyframes shooting {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(320px);
+  }
+}
+`
+const StarBackground = ({children}) => (
+ <SkyContainer>
+  <div className="sky-container">
+    <div className="star"></div>
+    <div className="star"></div>
+    <div className="star"></div>
+    <div className="star"></div>
+    <div className="star"></div>
+  </div>
+
+  {children}
+ </SkyContainer>
+
+)
+
+
+
+
 
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-  <Background>
+      <StarBackground>
 
-  <ContainerWrapper css={css`padding-top: 90px; text-align: center;`}>
-      <H1>Discover Brüner Satellite <br/> Internet Bundles</H1>
-      <p css={css`color: white;`}>Jimmy B is here to make the world a better place, one connection at a time.</p>
-      <div css={css`margin-top: 50px;`}>
-        <Input type='tel' placeholder="Enter Zip" />
-        <ZipButton>Connect with the bruner</ZipButton>
-      </div>
+        <ContainerWrapper>
+            <H1>Discover Brüner Satellite <br/> Internet Bundles</H1>
+            <p css={css`color: white;`}>Jimmy B is here to make the world a better place, one connection at a time.</p>
+            <div css={css`margin-top: 50px;`}>
+              <Input type='tel' placeholder="Enter Zip" />
+              <ZipButton>Connect with the bruner</ZipButton>
+            </div>
 
-  </ContainerWrapper>
-</Background>
+        </ContainerWrapper>
+    </StarBackground>
 
-
- <Wave />
-
-
+     <Wave />
 
     <ContainerWrapper largePadding>
       <H2 css={css`text-align: center;`}>Brüner’s Best Plans:</H2>
